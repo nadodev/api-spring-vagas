0e220640ea4aece2c5608b37f1280403617cfb56
leoanrdo <programacao@studioalpha.com.br>
2024-08-23 10:57:09 -0300

    fix: add UserFoundExceptions

---
+++ src/main/java/io/github/nadodev/gesta_vagas/exceptions/UserFoundExceptions.java
@@ -0,0 +1,7 @@
+package io.github.nadodev.gesta_vagas.exceptions;
+
+public class UserFoundExceptions extends RuntimeException {
+    public UserFoundExceptions(String message) {
+        super(message);
+    }
+}
