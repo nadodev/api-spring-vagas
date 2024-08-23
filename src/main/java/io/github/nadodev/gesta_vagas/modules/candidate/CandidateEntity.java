c8bac25e7a453d24659e4660f50f3eaf0d357f04
leoanrdo <programacao@studioalpha.com.br>
2024-08-23 10:40:43 -0300

    fix: change validation fields

--- src/main/java/io/github/nadodev/gesta_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/io/github/nadodev/gesta_vagas/modules/candidate/CandidateEntity.java
@@ -1,14 +1,20 @@
 package io.github.nadodev.gesta_vagas.modules.candidate;
 
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
 import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
+@Entity
+@Table(name = "candidate")
 public class CandidateEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     @NotNull
@@ -22,10 +28,14 @@ public class CandidateEntity {
     private String email;
 
     @NotBlank
-    @Size(min = 4, message = "O campo [Password] deve ter no mínimo 4 caracteres")
+    @Size(min = 4, message = "O campo [Password] deve ter no mínimo 4 caracteres", max = 100)
     private String password;
     private String description;
     private String curriculum;
+
+
+    @CreatedDate
+    private LocalDateTime createdAt;
 
 
 
