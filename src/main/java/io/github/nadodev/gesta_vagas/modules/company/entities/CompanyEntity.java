bbf8d52a2d20e049cfd2c6681e392b42a9ea29c3
leoanrdo <programacao@studioalpha.com.br>
2024-08-23 17:08:40 -0300

    feat: create job and company Entity

---
+++ src/main/java/io/github/nadodev/gesta_vagas/modules/company/entities/CompanyEntity.java
@@ -0,0 +1,42 @@
+package io.github.nadodev.gesta_vagas.modules.company.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "company")
+public class CompanyEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotBlank
+    @Pattern(regexp = "\\S+", message = "O campo [Username] não pode conter espaços em branco")
+    private String username;
+
+    @Email(message = "Email inválido")
+    private String email;
+
+    @NotBlank
+    @Size(min = 4, message = "O campo [Password] deve ter no mínimo 4 caracteres", max = 100)
+    private String password;
+    private String website;
+
+    @NotBlank
+    private String name;
+    private String description;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+}
---
+++ src/main/java/io/github/nadodev/gesta_vagas/modules/company/entities/JobEntity.java
@@ -0,0 +1,29 @@
+package io.github.nadodev.gesta_vagas.modules.company.entities;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "job")
+@Data
+public class JobEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String description;
+    private String benefits;
+    private String levels;
+
+    private UUID companyId;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+}
