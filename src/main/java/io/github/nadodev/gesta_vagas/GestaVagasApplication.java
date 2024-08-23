eb18bc3b92c2546b1c4f03b89ab106145ed12f4d
leoanrdo <programacao@studioalpha.com.br>
2024-08-23 09:24:35 -0300

    first commit

---
+++ src/main/java/io/github/nadodev/gesta_vagas/GestaVagasApplication.java
@@ -0,0 +1,13 @@
+package io.github.nadodev.gesta_vagas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaVagasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestaVagasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/nadodev/gesta_vagas/GestaVagasApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.nadodev.gesta_vagas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaVagasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
