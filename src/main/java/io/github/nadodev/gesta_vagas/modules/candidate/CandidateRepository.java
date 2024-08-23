6d095ef6633e5a1efd897a90d4c30b9f7ded0ee6
leoanrdo <programacao@studioalpha.com.br>
2024-08-23 10:57:32 -0300

    fix: change CandidateController and CandidateRepository

--- src/main/java/io/github/nadodev/gesta_vagas/modules/candidate/CandidateRepository.java
+++ src/main/java/io/github/nadodev/gesta_vagas/modules/candidate/CandidateRepository.java
@@ -2,7 +2,10 @@ package io.github.nadodev.gesta_vagas.modules.candidate;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface CandidateRepository  extends JpaRepository<CandidateEntity, UUID> {
+
+    Optional<CandidateEntity> findByUsernameOrEmail(String username, String email);
 }
--- src/main/java/io/github/nadodev/gesta_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/io/github/nadodev/gesta_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,6 +1,7 @@
 package io.github.nadodev.gesta_vagas.modules.candidate.controllers;
 
 
+import io.github.nadodev.gesta_vagas.exceptions.UserFoundExceptions;
 import io.github.nadodev.gesta_vagas.modules.candidate.CandidateEntity;
 import io.github.nadodev.gesta_vagas.modules.candidate.CandidateRepository;
 import jakarta.validation.Valid;
@@ -10,16 +11,24 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
 
-    @Autowired
-    private CandidateRepository candidateRepository;
+    private final CandidateRepository candidateRepository;
 
+    public CandidateController(CandidateRepository candidateRepository) {
+        this.candidateRepository = candidateRepository;
+    }
 
     @PostMapping("/")
     public void create(@Valid @RequestBody CandidateEntity candidateEntity) {
+        this.candidateRepository.findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail()).ifPresent(candidate -> {
+            throw new UserFoundExceptions("Username or email already exists");
+        });
+
         candidateRepository.save(candidateEntity);
     }
 }
