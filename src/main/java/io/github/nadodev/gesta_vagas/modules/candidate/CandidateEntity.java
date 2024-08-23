a48d58aeb802afe056de7f2c3bb6f9e5b8fb6bb5
leoanrdo <programacao@studioalpha.com.br>
2024-08-23 10:17:04 -0300

    fix: change validation fields

--- src/main/java/io/github/nadodev/gesta_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/io/github/nadodev/gesta_vagas/modules/candidate/CandidateEntity.java
@@ -1,9 +1,7 @@
 package io.github.nadodev.gesta_vagas.modules.candidate;
 
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 import java.util.UUID;
@@ -16,13 +14,15 @@ public class CandidateEntity {
     @NotNull
     private String name;
 
-    @NotNull
+    @NotBlank
+    @Pattern(regexp = "\\S+", message = "O campo [Username] não pode conter espaços em branco")
     private String username;
 
-    @Email(message = "Email should be valid")
+    @Email(message = "Email deve ser valido")
     private String email;
 
-    @NotNull
+    @NotBlank
+    @Size(min = 4, message = "O campo [Password] deve ter no mínimo 4 caracteres")
     private String password;
     private String description;
     private String curriculum;
